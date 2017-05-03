e18039905800283a6595f854990079992bc0d0dd
MicroBahner <fpm@mnet-online.de>
2017-05-03 21:58:25 +0200

    mit Ausblendung von 'NC' Ports
    NC-Pins werden im Sketch ausgeblended und die Arduino-Aufrufe pinMode()
    und digitalWrite() werden für diese Pins nicht ausgeführt, da die
    Prüfung auf gültige Pins dort nicht funktioniert.

--- DCC_Zubehoerdecoder/DCC_Zubehoerdecoder-STM32.h
+++ DCC_Zubehoerdecoder/DCC_Zubehoerdecoder-STM32.h
@@ -147,10 +147,10 @@ const byte modePin      =   33;     // Anzeige Betriebszustand (Normal/Programmi
 
 #define MAX_LEDS 12 // default ist 16. Kann auf die tatsächlich benutzte Zahl reduziert werden, um RAM zu sparen.
                     // Pro Softled werden 19 Byte benötigt
-/*
+
 const byte iniTyp[]     =   {    FCOIL,   FSIGNAL2, FSIGNAL0,   FVORSIG,  FSIGNAL0,          FSTATIC };
-const byte out1Pins[]   =   {       NC,         15,       18,       28,       31,               12 };  // output-pins der Funktionen
-const byte out2Pins[]   =   {       NC,         16,       NC,       29,       NC,               13 };
+const byte out1Pins[]   =   {        8,         15,       18,       28,       31,               12 };  // output-pins der Funktionen
+const byte out2Pins[]   =   {        9,         16,       NC,       29,       NC,               13 };
 const byte out3Pins[]   =   {       NC,         17,       NC,       30,       NC,               NC };
  
 const byte iniFmode[]     = { CAUTOOFF, LEDINVERT, 0b00000100,LEDINVERT,        0,  BLKMODE|BLKSOFT };
@@ -159,53 +159,4 @@ const byte iniPar2[]      = {       50, 0b0000001, 0b00001001,   0b1100,      25
 const byte iniPar3[]      = {        0,         4,          8,        8,        9,              100 };
 const byte iniPar4[]      = {        0, 0b0000101,          0,        0,        0,                0,}; // nur für Lichtsignale!
 
-*/
-const byte iniTyp[] = { FSTATIC, FSERVO };
-const byte out1Pins[] = { 15, 28 }; // output-pins der Funktionen
-const byte out2Pins[] = { NC, NC };
-const byte out3Pins[] = { NC, NC };
 
-const byte iniFmode[] = { BLKMODE|BLKSOFT, SAUTOOFF};
-const byte iniPar1[] = { 50, 70};
-const byte iniPar2[] = { 50, 110};
-const byte iniPar3[] = { 100, 8};
-const byte iniPar4[] = { 0, 0}; // nur für Lichtsignale!
-/*
-const byte iniTyp[]     =   {    FCOIL,   FSIGNAL2, FSIGNAL0,   FVORSIG,   FSERVO,          FSTATIC };
-const byte out1Pins[]   =   {       A2,          9,       12,        7,       A1,                5 };  // output-pins der Funktionen
-const byte out2Pins[]   =   {       A3,         10,       NC,        8,        3,                6 };
-const byte out3Pins[]   =   {       NC,         11,       NC,        NC,       NC,               NC };
- 
-// Funktionsspezifische Parameter. Diese Parameter beginnen bei CV 50 und pro Funktionsausgang gibt es
-// 5 CV-Werte. Die ersten 4 Werte steuern das Verhalten und in der folgenden Tabelle sind Erstinitiierungswerte
-// für diese CV's enthalten. Der 5. Wert dient internen Zwecken und wird hier nicht initiiert
-// In der Betriebsart 'INIMode' werden Mode und Parx Werte bei jedem Start aus der folgenden Tabelle übernommen
-// Die Tabellenwerte müssen an die Typaufteilung ( iniTyp, s.o.) angepasst werden.
-const byte iniFmode[]     = { CAUTOOFF, LEDINVERT, 0b00000100,        0,        0,  BLKMODE|BLKSOFT };
-const byte iniPar1[]      = {       50, 0b0000010, 0b00000100,   0b0001,        0,               50 };
-const byte iniPar2[]      = {       50, 0b0000001, 0b00001001,   0b0010,      180,               50 };
-const byte iniPar3[]      = {        0,         4,          8,        8,        8,              100 };
-const byte iniPar4[]      = {        0, 0b0000101,          0,        0,        0,                0,}; // nur für Lichtsignale!
-
-//------------------------------------------------------------------------------------
-/* die folgenden Werte dienen als Beispiele für sinnvolle Einträge in der obigen Paramtertabelle. 
-// Sie werden im Programm nicht direkt verwendet!
-// Standardwerte für Servoausgang 
-const byte iniServoMode     = SAUTOOFF;     // = (Mode) automatische Pulsabschaltung
-const byte iniServoGerade   = 0;     // = Par1;
-const byte iniServoAbzw     = 180;   // = Par2;
-const byte inispeed = 8;             // = Par3;
-
-// Standardwerte für Puls-Ausgang (Doppelspule)
-const byte iniCoilMode     =  CAUTOOFF;    // = (Mode) automatische Pulsbegrenzung eingeschaltet (0=AUS)
-const byte iniCoilOn       = 50;    // = (Par1) 500ms Impuls
-const byte iniCoilOff      = 20;    // = (Par2) mindestens 2Sec Pause zwischen 2 Pulsen
-
-// Standardwerte für statisch/Blinkende Ausgänge
-// sind 2 Ausgänge definiert (out1Pins, out2Pins), arbeiten sie im Gegentakt.
-const byte iniStaticMode    = BLINKMODE; // Bit0: Blinken/Statisch
-                                         // Bit1: Beim Blinken starten erst beide Leds dann Wechselblinken
-                                         // Bit2: mit weichem Auf/Abblenden (Pins müssen PWM-fähig sein)
-const byte iniBlinkOn       = 100;   // = (Par1) 1sec ein
-const byte iniBlinkOff      = 50 ;   // = (Par2) 0,5sec aus
-*/
