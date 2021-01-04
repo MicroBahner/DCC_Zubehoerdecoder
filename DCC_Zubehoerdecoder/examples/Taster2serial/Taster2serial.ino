d9e1c963a4cb042f1d195fc2b04a56dc0b3723db
Franz-Peter <fpm-gh@mnet-mail.de>
2021-01-04 16:19:39 +0100

    Update Taster2serial.ino

--- DCC_Zubehoerdecoder/examples/Taster2serial/Taster2serial.ino
+++ DCC_Zubehoerdecoder/examples/Taster2serial/Taster2serial.ino
@@ -1,14 +1,18 @@
-// Tastendrücke in serielle KOmmandos für DIY-Decoder umwandeln
-#define MAX8BUTTONS
+// Tastendrücke in serielle Kommandos für DIY-Decoder umwandeln
+#define MAX32BUTTONS    // es können bis zu 32 Taster definiert wrden
 #include <MobaTools.h>
-// define pin numbers
-const byte buttonPin [] = { 2,3,4,5 };
+#define SERIAL Serial   // verwendete Schnittstelle
+// define pin numbers and assigned adresses/states
+const byte buttonPin [] = { 2, 3, 4, 5 };
+const byte adresses [] =  {17,17,18,18 };   // DCC-adresses ( Output  adressing )
+const byte states []   =  { 0, 1, 0, 1 };   // state ( 0 or 1 );
 const byte buttonCnt = sizeof(buttonPin);
 MoToButtons Buttons( buttonPin, buttonCnt, 30, 500, 400 );
+char cmdBuf[20];
 
 void setup()
 {
-  Serial.begin(115200);
+  SERIAL.begin(115200);
 }
 
 void loop() {
@@ -17,8 +21,14 @@ void loop() {
   Buttons.processButtons();
   // 
   //--------------------------------------------------------
-  if ( Buttons.pressed(0) ) Serial.println("ac 17 0 0");
-  if ( Buttons.pressed(1) ) Serial.println("ac 17 1 0");
-  if ( Buttons.pressed(2) ) Serial.println("ac 18 0 0");  
-  if ( Buttons.pressed(3) ) Serial.println("ac 18 1 0");
+  for ( byte i=0; i<buttonCnt; i++ ) {
+    if ( Buttons.pressed(i) ) {
+      sprintf(cmdBuf, "ac %d %d 1", adresses[i], states[i] );
+      SERIAL.println(cmdBuf);
+    }
+    if ( Buttons.released(i) ) {
+      sprintf(cmdBuf, "ac %d %d 0", adresses[i], states[i] );
+      SERIAL.println(cmdBuf);
+    }
+  }
 }
